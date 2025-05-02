970dab38109d97f5acd4ed363375314610d2dfbe
Kazuya Ueoka <info@fromkk.info>
2025-05-03 08:50:26 +0900

    Initial Commit

---
+++ Presentations/PresentationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationsApp.swift
+//  Presentations
+//
+//  Created by Kazuya Ueoka on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct PresentationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PresentationsTests/PresentationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationsTests.swift
+//  PresentationsTests
+//
+//  Created by Kazuya Ueoka on 2025/05/03.
+//
+
+import Testing
+@testable import Presentations
+
+struct PresentationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
